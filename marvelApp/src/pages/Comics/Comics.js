feea848c10fd7289a5674e16ccf27c7e0d8d5e55
Melis Nur KILIÇ <melisnurklc0@gmail.com>
2021-12-28 14:08:13 +0300

    Project structure / navigation operations done

---
+++ marvelApp/src/pages/Comics/Comics.js
@@ -0,0 +1,10 @@
+import {useNavigation} from '@react-navigation/native';
+import React from 'react';
+import {View, Text} from 'react-native';
+import ComicsLayout from '../../components/ComicsLayout';
+
+const Comics = () => {
+  return <ComicsLayout />;
+};
+
+export default Comics;
